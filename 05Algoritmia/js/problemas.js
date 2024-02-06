e3f13ab4521f3145e5c9ce1efbb453a6c2acb4bd
demonassit <jminorg@ipn.mx>
2024-02-06 10:26:12 -0600

    algoritmia quedan 2 de tarea

---
+++ 05Algoritmia/js/problemas.js
@@ -0,0 +1,47 @@
+
+
+//problema 1
+
+
+//problema 2
+
+function problema2(){
+
+    //obtener los parametros de las entradas de datos
+    var p2_x1 = document.querySelector('#p2-x1').value;
+    var p2_x2 = document.querySelector('#p2-x2').value;
+    var p2_x3 = document.querySelector('#p2-x3').value;
+    var p2_x4 = document.querySelector('#p2-x4').value;
+    var p2_x5 = document.querySelector('#p2-x5').value;
+
+    var p2_y1 = document.querySelector('#p2-y1').value;
+    var p2_y2 = document.querySelector('#p2-y2').value;
+    var p2_y3 = document.querySelector('#p2-y3').value;
+    var p2_y4 = document.querySelector('#p2-y4').value;
+    var p2_y5 = document.querySelector('#p2-y5').value;
+
+    //construir el vector
+    var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+    var v2 = [p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+
+    //debo de darle un ordenamiento al vector
+    v1 = v1.sort(function(a,b){
+        return b-a;
+    });
+    v2 = v2.sort(function(a,b){
+        return b-a;
+    });
+
+    v2 = v2.reverse();
+
+    var p2_producto = 0;
+
+    for(var i = 0; i < v1.length; i++){
+        p2_producto += v1[i] * v2[i];
+    }
+
+    document.querySelector('#p2-output').textContent = 'Producto Escalar Minimo : ' + p2_producto;
+
+}
+
+//problema 3
