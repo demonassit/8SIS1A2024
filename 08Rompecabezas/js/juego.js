11cdf5441e8c530e9288f6607a1598bcd54a61fe
demonassit <jminorg@ipn.mx>
2024-02-22 08:56:31 -0600

    reglas de las piezas

--- 08Rompecabezas/js/juego.js
+++ 08Rompecabezas/js/juego.js
@@ -39,3 +39,124 @@ function mostrarInstruccionEnLista(instruccion, idLista){
     li.textContent = instruccion;
     ul.appendChild(li);
 }
+
+//vamos hacer una funcion para agregar la ultima direccion del movimiento
+function agregarUltimoMovimiento(direccion){
+    movimientos.push(direccion);
+}
+
+//necesitamos una funcion que se encargue de saber si gane
+function checarSiGano(){
+    for(var i = 0; i < rompe.length; i++){
+        for(var j = 0; j < rompe[i].length; j++){
+            var rompeActual = rompe[i][j];
+            if(rompeActual !== rompeCorrecta[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+//funcion para decir que gane
+function mostrarCartelGanador(){
+    if(checarSiGano()){
+        alert("Wiiii a mimir");
+    }
+    return false;
+}
+
+/*
+Debo integrar una funcion que sirva para poder intercambiar las posiciones de la pieza
+entendiendo que tengo lo siguiente:
+arreglo[1][2] = arreglo[0][0]
+arreglo[0][0] = arreglo[1][2]
+*/
+
+function intercambiarPosicionesRompe(filaPos1, columnaPos1, filaPos2, columnaPos2){
+    var pos1 = rompe[filaPos1][columnaPos1];
+    var pos2 = rompe[filaPos2][columnaPos2];
+
+    //intercambio
+    rompe[filaPos1][columnaPos1] = pos2;
+    rompe[filaPos2][columnaPos2] = pos1;
+}
+
+function actualizarPosicionVacia(nuevaFila, nuevaColumna){
+    filaVacia = nuevaFila;
+    columnaVacia = nuevaColumna;
+}
+
+//tenemos que checar si la posicion dentro del compecabezas es la correcta
+function posicionValida(fila, columna){
+    return(fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2);
+}
+
+//ahora viene la parte del movimiento de las piezas, derivado a que tenemos que simular que se mueve o intercambia la posicion de cada pieza respecto de la pieza vacia, eso se debe de representar con el movimiento de las flechas de navegacion identificando que las teclas son: arriba(38), abajo(40), izquierda(37), derecha(39)
+
+var codigosDireccion = {
+    IZQUIERDA : 37,
+    ARRIBA : 38,
+    DERECHA : 39,
+    ABAJO : 40
+};
+
+function moverEnDireccion(direccion){
+    var nuevaFilaPiezaVacia;
+    var nuevaColumnaPiezaVacia;
+
+    //si se mueve hacia abajo
+    if(direccion === codigosDireccion.ABAJO){
+        nuevaFilaPiezaVacia = filaVacia + 1;
+        nuevaColumnaPiezaVacia = columnaVacia;
+    }
+    //si se mueve hacia arriba
+    if(direccion === codigosDireccion.ARRIBA){
+        nuevaFilaPiezaVacia = filaVacia - 1;
+        nuevaColumnaPiezaVacia = columnaVacia;
+    }
+    //si se mueve hacia derecha
+    if(direccion === codigosDireccion.DERECHA){
+        nuevaFilaPiezaVacia = filaVacia;
+        nuevaColumnaPiezaVacia = columnaVacia + 1;
+    }
+    //si se mueve hacia izquierda
+    if(direccion === codigosDireccion.IZQUIERDA){
+        nuevaFilaPiezaVacia = filaVacia;
+        nuevaColumnaPiezaVacia = columnaVacia - 1;
+    }
+
+    //checar si la nueva posicion es valida y sino intercambiarla
+    if(posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)){
+        intercambiarPosiciones(filaVacia, columnaVacia, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+        actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+        //agregar cual fue el ultimo movimiento
+        agregarUltimoMovimiento(direccion);
+    }
+
+}
+
+function intercambiarPosiciones(fila1, columna1, fila2, columna2){
+    //intercambio las posiciones de la pieza
+    var pieza1 = rompe[fila1][columna1];
+    var pieza2 = rompe[fila2][columna2];
+
+    intercambiarPosicionesRompe(fila1, columna1, fila2, columna2);
+    //para hacerlo visible en el html
+    intercambiarPosicionesDOM('pieza' + pieza1, 'pieza' + pieza2);
+}
+
+function intercambiarPosicionesDOM(idPieza1, idPieza2){
+    //necesito obtener los valores de las piezas del html
+    var elementoPieza1 = document.getElementById(idPieza1);
+    var elementoPieza2 = document.getElementById(idPieza2);
+
+    var padre = elementoPieza1.parentNode;
+
+    //clonar el elemento
+    var clonElemento1 = elementoPieza1.cloneNode(true);
+    var clonElemento2 = elementoPieza2.cloneNode(true);
+
+    padre.replaceChild(clonElemento1, elementoPieza2);
+    padre.removeChild(clonElemento2, elementoPieza1);
+}
